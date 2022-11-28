aebcc66cbb50b9e8d9e477c175ade1ba198fa5dd
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-11-29 00:44:39 +0900

    Create 1.5.js

---
+++ securecoding/src/main/resources/static/bootstrap/js/1.5.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var vulnerable5 = $("#vulnerable5");
+
+    $("#execute").click(function () {
+        var formData = new FormData(vulnerable5);
+        formData.append("cmd", )
+    })
+})
