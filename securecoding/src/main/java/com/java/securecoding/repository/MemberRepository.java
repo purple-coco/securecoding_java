f42146314de81a6ca031dbf906957abda9e87bd2
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-07 09:58:57 +0900

    Update MemberRepository.java

--- securecoding/src/main/java/com/java/securecoding/repository/MemberRepository.java
+++ securecoding/src/main/java/com/java/securecoding/repository/MemberRepository.java
@@ -8,5 +8,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
+    boolean existsByUsername(String username);
+
     Optional<Member> findByUsername(String username);
 }
