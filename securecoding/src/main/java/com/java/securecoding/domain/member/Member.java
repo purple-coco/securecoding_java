bd3acb05528602f1ab90181f12ad684b32885683
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-07 09:37:24 +0900

    Update Member.java

--- securecoding/src/main/java/com/java/securecoding/domain/member/Member.java
+++ securecoding/src/main/java/com/java/securecoding/domain/member/Member.java
@@ -1,4 +1,28 @@
 package com.java.securecoding.domain.member;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private int count;
+
+    private boolean islocked = false;
+
 }
