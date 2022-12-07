2afc23b2140eacf42d4e62c02d2daaaf99b416e3
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-07 09:58:52 +0900

    Update HomeController.java

--- securecoding/src/main/java/com/java/securecoding/controller/HomeController.java
+++ securecoding/src/main/java/com/java/securecoding/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.java.securecoding.controller;
 
+import com.java.securecoding.domain.form.MemberForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +12,13 @@ public class HomeController {
     @RequestMapping("/")
     public String main() {
         return "index";
+    }
+
+    @GetMapping(value = {"/member/login"})
+    public String LoginForm(Model model) {
+
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "/2/2.5";
     }
 }
