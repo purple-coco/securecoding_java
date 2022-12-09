d177b3b400ae5877a7533ddc600d4bf751ccbbe7
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-09 13:31:43 +0900

    Create MemberLoginForm.java

---
+++ securecoding/src/main/java/com/java/securecoding/domain/member/MemberLoginForm.java
@@ -0,0 +1,11 @@
+package com.java.securecoding.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberLoginForm {
+    private String name;
+    private String password;
+}
