fb801da053ae96a03c89456aa366784f8eca473b
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-09 09:05:30 +0900

    Update MemberService.java

--- securecoding/src/main/java/com/java/securecoding/service/MemberService.java
+++ securecoding/src/main/java/com/java/securecoding/service/MemberService.java
@@ -5,6 +5,7 @@ import com.java.securecoding.domain.member.Member;
 import com.java.securecoding.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Long join(Member member) {
+        validateDuplicateMember(member.getUsername());
         memberRepository.save(member);
 
         return member.getId();
@@ -31,17 +33,27 @@ public class MemberService {
     }
 
 //    안전한 비밀번호 생성 규칙 적용
-    public boolean passwordValidate(String password) {
+    public boolean passwordValidate(String inputPassword) {
         String passwordPolicy = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$";
 
         Pattern pattern = Pattern.compile(passwordPolicy);
-        Matcher matcher = pattern.matcher(password);
+        Matcher matcher = pattern.matcher(inputPassword);
 
         return matcher.matches();
     }
 
-    public String encryptedPassword(String password) {
-        return "";
+    public String HashPassword(String inputPassword) {
+        String encryptedPassword = BCrypt.hashpw(inputPassword, BCrypt.gensalt(10));
+
+        return encryptedPassword;
+    }
+
+    public boolean checkPassword(String inputPassword, String encryptedPassword) {
+        if (BCrypt.checkpw(inputPassword, encryptedPassword)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
