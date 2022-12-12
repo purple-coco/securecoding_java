bc73bf402238be6cda63d8744ae1a1693fa4b3ef
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-12 13:44:53 +0900

    Create MemberInfo.java

---
+++ securecoding/src/main/java/com/java/securecoding/domain/session/MemberInfo.java
@@ -0,0 +1,11 @@
+package com.java.securecoding.domain.session;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberInfo {
+    private Long id;
+    private String username;
+}
