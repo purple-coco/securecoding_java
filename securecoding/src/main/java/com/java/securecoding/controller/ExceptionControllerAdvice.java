835c6322d144f0afa25dbc6caafda7bade79590d
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-12 13:44:49 +0900

    Update ExceptionControllerAdvice.java

--- securecoding/src/main/java/com/java/securecoding/controller/ExceptionControllerAdvice.java
+++ securecoding/src/main/java/com/java/securecoding/controller/ExceptionControllerAdvice.java
@@ -1,17 +1,26 @@
 package com.java.securecoding.controller;
 
 import exception.NotJoinException;
+import exception.PermissionException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionController {
+public class ExceptionControllerAdvice {
 
     @ExceptionHandler(NotJoinException.class)
     public String redirectJoin(Model model) {
         model.addAttribute("message", "중복된 아이디입니다.");
         model.addAttribute("searchUrl", "/member/signup");
+
+        return "message";
+    }
+
+    @ExceptionHandler(PermissionException.class)
+    public String NotPermission(Model model) {
+        model.addAttribute("message", "정상적인 접근이 아닙니다.");
+        model.addAttribute("searchUrl", "/");
 
         return "message";
     }
