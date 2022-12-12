1ff3f35a7541ae62ee566651350a8379928bafa5
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-12 13:45:45 +0900

    Update MemberLoginForm.java

--- securecoding/src/main/java/com/java/securecoding/domain/member/MemberLoginForm.java
+++ securecoding/src/main/java/com/java/securecoding/domain/member/MemberLoginForm.java
@@ -3,9 +3,10 @@ package com.java.securecoding.domain.member;
 import lombok.Getter;
 import lombok.Setter;
 
+/*제2절 보안기능 16.반복된 인증시도 제한 기능 부재 Vulnerable 로그인 폼*/
 @Getter
 @Setter
 public class MemberLoginForm {
-    private String name;
+    private String username;
     private String password;
 }
