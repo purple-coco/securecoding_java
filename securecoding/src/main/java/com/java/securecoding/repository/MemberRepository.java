b490539b5c6e367cd6c6470f1d5d5657e4164619
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-13 11:04:41 +0900

    Update MemberRepository.java

--- securecoding/src/main/java/com/java/securecoding/repository/MemberRepository.java
+++ securecoding/src/main/java/com/java/securecoding/repository/MemberRepository.java
@@ -2,8 +2,13 @@ package com.java.securecoding.repository;
 
 import com.java.securecoding.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
@@ -11,5 +16,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByUsername(String username);
 
-//    Optional<Member> findByUsername(String username);
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Member m SET m.count = m.count + 1 WHERE m.username = :username")
+    int updateCountFailure(@Param("username") String username);
+
 }
