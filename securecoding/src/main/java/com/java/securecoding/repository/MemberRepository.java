67582f3516a1d09377ee3eb26737be23094b5026
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-15 08:32:53 +0900

    Update MemberRepository.java

--- securecoding/src/main/java/com/java/securecoding/repository/MemberRepository.java
+++ securecoding/src/main/java/com/java/securecoding/repository/MemberRepository.java
@@ -7,10 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+
+    Member findAllById(Long id);
 
     boolean existsByUsername(String username);
 
