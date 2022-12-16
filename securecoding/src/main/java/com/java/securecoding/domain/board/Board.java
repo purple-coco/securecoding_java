d5063abf7e48cd2270132472af9096e0ee3f5a0c
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-16 16:56:23 +0900

    Update Board.java

--- securecoding/src/main/java/com/java/securecoding/domain/board/Board.java
+++ securecoding/src/main/java/com/java/securecoding/domain/board/Board.java
@@ -1,4 +1,50 @@
 package com.java.securecoding.domain.board;
 
+import com.java.securecoding.domain.member.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
 public class Board {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "board_id")
+    private Long id;
+
+    private String subject;
+
+    private String content;
+
+    private LocalDate createDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private boolean isDelete = false;
+
+    private String fileName;
+
+    private String filePath;
+
+    public static Board createBoard(Member member, String subject, String content, String fileName, String filePath) {
+
+        Board board = new Board();
+
+        board.setMember(member);
+        board.setSubject(subject);
+        board.setContent(content);
+        board.setCreateDate(LocalDate.now());
+        board.setFileName(fileName);
+        board.setFilePath(filePath);
+
+        return board;
+    }
+
 }
