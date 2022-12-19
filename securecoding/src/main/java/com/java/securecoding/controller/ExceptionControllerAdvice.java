8ae87f43beaba06f4b4abdadea650e27cfb1382a
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-19 10:22:49 +0900

    Update ExceptionControllerAdvice.java

--- securecoding/src/main/java/com/java/securecoding/controller/ExceptionControllerAdvice.java
+++ securecoding/src/main/java/com/java/securecoding/controller/ExceptionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.java.securecoding.controller;
 
 import exception.NotJoinException;
 import exception.PermissionException;
+import exception.UpdateInfoException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,6 +21,14 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(PermissionException.class)
     public String NotPermission(Model model) {
         model.addAttribute("message", "권한이 없습니다.");
+        model.addAttribute("searchUrl", "/");
+
+        return "message";
+    }
+
+    @ExceptionHandler(UpdateInfoException.class)
+    public String UpdateMemberInfo(Model model) {
+        model.addAttribute("message", "비밀번호는 최소 8자 이상, 영어 대·소문자, 숫자, 특수문자가 혼용되어야 합니다.");
         model.addAttribute("searchUrl", "/");
 
         return "message";
