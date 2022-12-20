ef9017ee246e81c8759d9d346faf895f09e98576
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-20 13:56:11 +0900

    Update ExceptionControllerAdvice.java

--- securecoding/src/main/java/com/java/securecoding/controller/ExceptionControllerAdvice.java
+++ securecoding/src/main/java/com/java/securecoding/controller/ExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.java.securecoding.controller;
 
+import exception.NotAllowExtException;
 import exception.NotJoinException;
 import exception.PermissionException;
 import exception.UpdateInfoException;
@@ -31,6 +32,14 @@ public class ExceptionControllerAdvice {
     public String UpdateMemberInfo(@PathVariable Long memberId, Model model) {
         model.addAttribute("message", "비밀번호는 최소 8자 이상, 영어 대·소문자, 숫자, 특수문자가 혼용되어야 합니다.");
         model.addAttribute("searchUrl", "/mypage/info/" + memberId);
+
+        return "message";
+    }
+
+    @ExceptionHandler(NotAllowExtException.class)
+    public String NotAllowExt(Model model) {
+        model.addAttribute("message", "허용되지 않는 파일 확장자입니다.");
+        model.addAttribute("searchUrl", "/board/new");
 
         return "message";
     }
