20f0c8c088e35d0023a95df653e78a8136acd1c5
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-23 15:22:28 +0900

    Update BoardController.java

--- securecoding/src/main/java/com/java/securecoding/controller/BoardController.java
+++ securecoding/src/main/java/com/java/securecoding/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.java.securecoding.controller;
 
 import com.java.securecoding.domain.board.Board;
 import com.java.securecoding.domain.form.BoardForm;
+import com.java.securecoding.domain.form.BoardForm2;
 import com.java.securecoding.domain.member.Member;
 import com.java.securecoding.domain.session.MemberInfo;
 import com.java.securecoding.service.BoardService;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -25,6 +27,16 @@ public class BoardController {
 
     private final MemberService memberService;
     private final BoardService boardService;
+
+    @GetMapping(value = {"/board", "/1/1"})
+    public String BoardListForm(Model model) {
+
+        List<Board> board = boardService.findAll();
+
+        model.addAttribute("boards", board);
+
+        return "/1/1.1";
+    }
 
     @GetMapping(value = {"/board/new", "/1/6/vuln", "/1/6/secure"})
     public String CreateBoardForm(Model model) {
@@ -53,13 +65,6 @@ public class BoardController {
                 form.getFileName(),
                 form.getFilePath());
 
-        System.out.println("board = " + board);
-        System.out.println("board.getContent() = " + board.getContent());
-        System.out.println("board.getSubject() = " + board.getSubject());
-        System.out.println("board.getFileName() = " + board.getFileName());
-
-        System.out.println("board.getFilePath() = " + board.getFilePath());
-
         boardService.saveBoard(board, file);
 
         model.addAttribute("message", "글 등록이 완료되었습니다.");
@@ -69,7 +74,7 @@ public class BoardController {
     }
 
     @PostMapping("/1/6/secure")
-    public String CreateBoardForm_secure(BoardForm form, Model model,
+    public String CreateBoardForm_secure(BoardForm2 form, Model model,
                                          MultipartHttpServletRequest request, MultipartFile file) throws IOException {
         HttpSession session = request.getSession();
 
@@ -82,13 +87,6 @@ public class BoardController {
                 form.getContent(),
                 form.getFileName(),
                 form.getFilePath());
-
-        System.out.println("board.getContent() = " + board.getContent());
-        System.out.println("board.getSubject() = " + board.getSubject());
-        System.out.println("board.getFileName() = " + board.getFileName());
-
-        System.out.println("board.getFilePath() = " + board.getFilePath());
-
 
         boardService.saveBoard_secure(board, file);
 
