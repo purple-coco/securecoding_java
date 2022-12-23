54cee6ee2496ccfeaad24c3c3c17187d66545064
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-23 15:22:40 +0900

    Update BoardService.java

--- securecoding/src/main/java/com/java/securecoding/service/BoardService.java
+++ securecoding/src/main/java/com/java/securecoding/service/BoardService.java
@@ -2,6 +2,7 @@ package com.java.securecoding.service;
 
 import com.java.securecoding.domain.board.Board;
 import com.java.securecoding.repository.BoardRepository;
+import exception.NotAllowExtException1;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,24 +10,70 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BoardService {
+
+    private final BoardRepository boardRepository;
 
     @Value(("${file.dir"))
     private String fileDir;
 
-    private final BoardRepository boardRepository;
+    /* 게시판 목록 */
+    public List<Board> findAll() {
+        return boardRepository.findAll();
+    }
 
     @Transactional
-    public Long saveBoard(Board board, MultipartFile file) {
+    public Long saveBoard(Board board, MultipartFile file) throws IOException {
         String originalFilename = "";
         String filePath = "";
 
-        if(!file.isEmpty()) {
+        if (!file.isEmpty()) {
             originalFilename = file.getOriginalFilename();
             filePath = fileDir + originalFilename;
+
+            log.info("파일 저장 {}", filePath);
+            file.transferTo(new File(filePath));
+            log.info("저장 완료");
+
+    }
+            board.setFileName(originalFilename);
+            board.setFilePath(filePath);
+
+        boardRepository.save(board);
+
+        return board.getId();
+    }
+
+    @Transactional
+    public Long saveBoard_secure(Board board, MultipartFile file) throws IOException {
+        String originalFilename = " ";
+        String filePath = " ";
+
+        if(!file.isEmpty()) {
+
+            String originalFileExt = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+
+            if (!file.getOriginalFilename().matches("[\\.\\%\\;\\/\\/]*")) {
+                throw new NotAllowExtException1("파일 이름에 허용하지 않는 문자열이 포함되었습니다.");
+            }
+            if (originalFileExt.equals("jpg") || originalFileExt.equals("png")
+                    || originalFileExt.equals("docx") || originalFileExt.equals("jpeg")) {
+
+                originalFilename = file.getOriginalFilename();
+                filePath = fileDir + originalFilename;
+
+                file.transferTo(new File(filePath));
+            } else {
+                throw new NotAllowExtException1("업로드 허용되는 파일 확장자가 아닙니다.");
+            }
         }
 
         board.setFileName(originalFilename);
