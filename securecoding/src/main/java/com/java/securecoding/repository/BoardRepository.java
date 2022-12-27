76bb3827d170456e58085c8cdde6a98db943d2f8
purple-coco <71930627+purple-coco@users.noreply.github.com>
2022-12-27 16:12:33 +0900

    Update BoardRepository.java

--- securecoding/src/main/java/com/java/securecoding/repository/BoardRepository.java
+++ securecoding/src/main/java/com/java/securecoding/repository/BoardRepository.java
@@ -1,9 +1,12 @@
 package com.java.securecoding.repository;
 
 import com.java.securecoding.domain.board.Board;
+import com.java.securecoding.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    Board findAllById(Long id);
 }
