21c8ed6c8f255c0f2641849607152b2d3f8fb85a
purple-coco <71930627+purple-coco@users.noreply.github.com>
2023-01-01 16:09:20 +0900

    Update LoginCheckInterceptor.java

--- securecoding/src/main/java/com/java/securecoding/interceptor/LoginCheckInterceptor.java
+++ securecoding/src/main/java/com/java/securecoding/interceptor/LoginCheckInterceptor.java
@@ -1,6 +1,6 @@
 package com.java.securecoding.interceptor;
 
-import exception.PermissionException;
+import com.java.securecoding.exception.PermissionException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
