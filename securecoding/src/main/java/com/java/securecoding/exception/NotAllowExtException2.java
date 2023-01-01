b3e721c2e7f205613e2bd1e05e2b486d2153f755
purple-coco <71930627+purple-coco@users.noreply.github.com>
2023-01-01 16:09:07 +0900

    Create NotAllowExtException2.java

---
+++ securecoding/src/main/java/com/java/securecoding/exception/NotAllowExtException2.java
@@ -0,0 +1,7 @@
+package com.java.securecoding.exception;
+
+public class NotAllowExtException2 extends RuntimeException{
+    public NotAllowExtException2(String message) {
+        super(message);
+    }
+}
