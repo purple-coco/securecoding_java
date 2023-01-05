b1cad7948aec94c0986c9921cdd9603f0afb28cf
purple-coco <71930627+purple-coco@users.noreply.github.com>
2023-01-05 11:31:22 +0900

    Update TimeStatusController.java

--- securecoding/src/main/java/com/java/securecoding/controller/TimeStatusController.java
+++ securecoding/src/main/java/com/java/securecoding/controller/TimeStatusController.java
@@ -14,11 +14,6 @@ public class TimeStatusController {
         return "/3/3.1";
     }
 
-    @GetMapping("/3/1/code")
-    public String RaceConditonForm_code() {
-        return "/3/3.1.code";
-    }
-
     @GetMapping("/3/2")
     public String RecursiveFuntionForm() {
         return "/3/3.2";
